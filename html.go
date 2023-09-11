84f2105486b017345842b16ee2a69b7ffae31eb3
shankusu <shankusu2017@gmail.com>
2023-09-11 17:54:22 +0800

    little

--- html.go
+++ html.go
@@ -8,6 +8,6 @@ import (
 func rspHome(c *gin.Context) {
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "不要因为走的太远而忘了为什么出发",
+		"title": "不要因为走的太远，而忘了为什么出发。",
 	})
 }
