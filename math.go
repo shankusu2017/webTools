149da58b7d1bf90b75f561c309d7895fd80bb262
shankusu <shankusu2017@gmail.com>
2023-11-28 15:31:51 +0800

    little

--- math.go
+++ math.go
@@ -7,12 +7,17 @@ import (
 	"strings"
 )
 
+const signedCharst = "01234567"
 const charset = "0123456789abcdef"
 
 func randomString(n int) string {
 	sb := strings.Builder{}
 	sb.Grow(n)
-	for i := 0; i < n; i++ {
+
+	// 限制第一个字符的范围，使产生的十六进制字符在 int 的范围内
+	// 可以直接在前面添加 "-" 而不用担心负数溢出的问题(eg: -f3876898)
+	sb.WriteByte(charset[rand.Intn(len(signedCharst))])
+	for i := 1; i < n; i++ {
 		sb.WriteByte(charset[rand.Intn(len(charset))])
 	}
 	return sb.String()
